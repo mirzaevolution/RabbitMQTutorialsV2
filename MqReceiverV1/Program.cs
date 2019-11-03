6ffd53757786add06ed90a430c38a03aeb8b9440
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2019-11-03 22:19:37 +0700

    Add project files.

---
+++ MqReceiverV1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MqReceiverV1
+{
+    class Program
+    {
+        static void Listen()
+        {
+            try
+            {
+                IConnectionFactory connectionFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost"
+                };
+                using(IConnection connection = connectionFactory.CreateConnection())
+                {
+                    using(IModel channel = connection.CreateModel())
+                    {
+                        //It will be created only if queue doesn't exist
+                        channel.QueueDeclare(
+                                queue: "queue_intro",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null
+                            );
+                        EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                        consumer.Registered += (sender, obj) =>
+                        {
+                            Console.WriteLine($"*** RabbitMQ Listener Registered ***");
+                        };
+                        consumer.Shutdown += (sender, obj) =>
+                        {
+                            Console.WriteLine("*** Shutting down RabbitMQ Listener ***");
+                        };
+                        consumer.Received += (sender, payload) =>
+                        {
+                            string message = Encoding.UTF8.GetString(payload.Body);
+                            Console.WriteLine($"Message: `{message}` was processed");
+                        };
+                        channel.BasicConsume(
+                                queue: "queue_intro",
+                                autoAck: true,
+                                consumer: consumer
+                            );
+                        Console.WriteLine("Press [ENTER] to quit");
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Listen();
+
+        }
+    }
+}
---
+++ MqSenderV1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+namespace MqSenderV1
+{
+    class Program
+    {
+        static void Send(string[] messages)
+        {
+            try
+            {
+                IConnectionFactory connectionFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost"
+                };
+                using(IConnection connection = connectionFactory.CreateConnection())
+                {
+                    using(IModel channel = connection.CreateModel())
+                    {
+
+                        //It will be created only if queue doesn't exist
+                        channel.QueueDeclare(
+                                queue: "queue_intro",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null
+                            );
+
+                        //Distribute all messages
+                        foreach(string message in messages)
+                        {
+                            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                            Console.WriteLine($"Sending message: `{message}`");
+                            channel.BasicPublish(
+                                    exchange: "",
+                                    routingKey: "queue_intro",
+                                    basicProperties: null,
+                                    body: messageBytes
+                                );
+                        }
+                        Console.WriteLine("All messsages sent successfully");
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Send(args == null || (args != null && args.Length == 0) ? new[] { "Hello World" } : args);
+        }
+    }
+}
